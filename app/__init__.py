c8dfefdc96cc22cd60716e17b83ecfa06d6e877d
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-29 06:24:37 +0900

    Add Secret Key to App Config

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # Change this to a secure key in production
+    app.config["SECRET_KEY"] = "potatoes"
+
     # Import and register blueprints
     from app.routes import main
     app.register_blueprint(main)

3362d99334f072ec1eba19def098aca9441753b5
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-05-30 04:34:47 +0900

    Add `authorize` Route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,12 @@
-from flask import Blueprint
+import os
+import secrets
+
+from dotenv import load_dotenv
+from flask import Blueprint, redirect, session
+
+load_dotenv()
+
+BASE_URL = "https://yutify.onrender.com"
 
 main = Blueprint("main", __name__)
 
@@ -6,3 +14,32 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return "Hello, World!"
+
+
+@main.route("/authorize")
+def authorize():
+    """
+    Redirect the user to the authorization endpoint
+    to ask for permission to access their data.
+    """
+
+    auth_endpoint = "/authorize"
+
+    client_id = os.getenv("CLIENT_ID")
+    redirect_uri = os.getenv("REDIRECT_URI")
+    response_type = "code"
+    scope = "activity"
+    state = secrets.token_urlsafe()
+
+    # Store the state in the session for later validation
+    session["state"] = state
+
+    # Construct the authorization URL with the necessary parameters
+    authorization_url = (
+        f"{BASE_URL}{auth_endpoint}?client_id={client_id}&redirect_uri={redirect_uri}"
+        f"&response_type={response_type}&scope={scope}&state={state}"
+    )
+
+    # Redirect the user to the authorization URL
+    # This is where the user will be asked to grant or deny access.
+    return redirect(authorization_url)
